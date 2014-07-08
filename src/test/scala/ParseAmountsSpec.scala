de29557aa2fc86ba128887b81c68069e99c47ce7
Cliff <cliffyschool@gmail.com>
2014-07-08 15:36:09 -0500

    reformat

--- src/test/scala/ParseAmountsSpec.scala
+++ src/test/scala/ParseAmountsSpec.scala
@@ -10,19 +10,19 @@ class ParseAmountsSpec extends Specification with DataTables{ def is =
   val parser = new Ingredients
 
   def e1 =
-    "line"   | "expectedAmount" | "expectedUnit" | "expectedIngredient" |
-      "2 cups butter"  !! 2.0d  ! "cups" ! "butter" |
-      "2 cups of butter"  !! 2.0d  ! "cups" ! "butter" |
-      "a cup of butter"  !! 1.0d  ! "cups" ! "butter" |
-      "1 cup butter"  !! 1.0d  ! "cups" ! "butter" |
-      "1/2 c. butter"  !! 0.5d  ! "cups" ! "butter" |
-      ".375 c. butter"  !! 0.375d  ! "cups" ! "butter" |
+    "line" | "expectedAmount" | "expectedUnit" | "expectedIngredient" |
+      "2 cups butter" !! 2.0d ! "cups" ! "butter" |
+      "2 cups of butter" !! 2.0d ! "cups" ! "butter" |
+      "a cup of butter" !! 1.0d ! "cups" ! "butter" |
+      "1 cup butter" !! 1.0d ! "cups" ! "butter" |
+      "1/2 c. butter" !! 0.5d ! "cups" ! "butter" |
+      ".375 c. butter" !! 0.375d ! "cups" ! "butter" |
       "2 10.5 oz cans diced tomatoes" !! 2d ! "10.5 oz cans" ! "diced tomatoes" |
       "2 10.5 oz. cans diced tomatoes" !! 2d ! "10.5 oz. cans" ! "diced tomatoes" |
       "2 10.5-oz. cans diced tomatoes" !! 2d ! "10.5-oz. cans" ! "diced tomatoes" |
       "2 10.5-oz cans diced tomatoes" !! 2d ! "10.5-oz cans" ! "diced tomatoes" |
       "2 10 1/2 oz cans diced tomatoes" !! 2d ! "10 1/2 oz cans" ! "diced tomatoes" |
-      "0.375 cup butter"  !! 0.375d  ! "cups" ! "butter"|> {
+      "0.375 cup butter" !! 0.375d ! "cups" ! "butter" |> {
       (line,expectedAmount,expectedUnit,expectedIngredient) => {parser.fromLine(line) must beSome(Ingredient(name = expectedIngredient, amount=expectedAmount, unit=expectedUnit))}
     }
 }
