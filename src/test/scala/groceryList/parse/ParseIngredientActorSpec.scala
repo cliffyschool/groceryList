7014299691c25ea6c2be600b24c263f811497816
cfreeman <cliffyschool@gmail.com>
2014-08-10 17:52:50 -0500

    got typed channels working in unit tests

--- src/test/scala/groceryList/parse/ParseIngredientActorSpec.scala
+++ src/test/scala/groceryList/parse/ParseIngredientActorSpec.scala
@@ -23,16 +23,16 @@ with Core {
   "groceryList.parse actor" should {
 
     sequential
+    implicit val selfChannel: ChannelRef[(Any, Nothing) :+: TNil] = new ChannelRef(testActor)
 
     "send a parsed ingredient message for a valid line" in {
-        ParseIngredient("1 cup butter") -?-> parseSystem
-        expectMsgType[IngredientParsed](FiniteDuration(5, TimeUnit.SECONDS)) must not beNull
+        parseSystem <-!- ParseIngredient("1 cup butter")
+        expectMsgType[IngredientParsed] must not beNull
     }
 
     "send a no-parsed-ingredient message for a blank line" in {
-      //parseSystem ! ParseIngredient("")
-     // expectMsgType[NoIngredientParsed] must not beNull
-      1 must equalTo(1)
+      parseSystem <-!- ParseIngredient("")
+      expectMsgType[NoIngredientParsed] must not beNull
     }
   }
 }
